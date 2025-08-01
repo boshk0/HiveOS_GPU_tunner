6d2879e4acc734a52602e97320fa38b4080ff85f
Bojidar Markov <75314475+boshk0@users.noreply.github.com>
2025-08-01 20:23:04 +0300

    Update setup_prometheus_exporters.sh

--- setup_prometheus_exporters.sh
+++ setup_prometheus_exporters.sh
@@ -27,8 +27,8 @@ WantedBy=multi-user.target
 EOF
 
 sudo systemctl daemon-reload;
-sudo systemctl start node_exporter;
 sudo systemctl enable node_exporter;
+sudo systemctl start node_exporter;
 sudo systemctl status node_exporter;
 
 cd ..
@@ -39,6 +39,7 @@ sudo rm tmp -r
 sudo wget -q -O /usr/local/bin/nvidia_gpu_exporter.sh https://raw.githubusercontent.com/boshk0/gddr6_temps/master/nvidia_gpu_exporter.sh;
 sudo chmod +x /usr/local/bin/nvidia_gpu_exporter.sh;
 sudo wget -q -O /etc/systemd/system/nvidia_gpu_exporter.service https://raw.githubusercontent.com/boshk0/gddr6_temps/master/nvidia_gpu_exporter.service;
+
 sudo systemctl daemon-reload;
 sudo systemctl enable nvidia_gpu_exporter;
 sudo systemctl start nvidia_gpu_exporter;
